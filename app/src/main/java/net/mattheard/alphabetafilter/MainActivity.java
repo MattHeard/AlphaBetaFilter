8fc01e80e5f79783f8464e3c18eb58d56ffa050d
Matt Heard <matt@mattheard.net>
2019-05-29 19:32:04 +0200

    Inline list creation

--- app/src/main/java/net/mattheard/alphabetafilter/MainActivity.java
+++ app/src/main/java/net/mattheard/alphabetafilter/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,10 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        List<String> sensors = new ArrayList<>();
-        sensors.add("io 1");
-        sensors.add("io 2");
-        sensors.add("io 3");
+        List<String> sensors = Arrays.asList("io 1", "io 2", "io 3");
         ArrayAdapter<String> sensorsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, sensors);
         sensorsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner sensorSpinner = findViewById(R.id.sensorsSpinner);
