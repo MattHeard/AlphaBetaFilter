6ea2f548d1dc45ccc0935eb07547c3ebed50022b
Matt Heard <matt@mattheard.net>
2019-05-29 19:28:45 +0200

    Add sensor spinner
    - Create configuration layout
    - Add spinner
    - Add fake sensors

--- app/src/main/java/net/mattheard/alphabetafilter/MainActivity.java
+++ app/src/main/java/net/mattheard/alphabetafilter/MainActivity.java
@@ -2,6 +2,11 @@ package net.mattheard.alphabetafilter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> sensors = new ArrayList<>();
+        sensors.add("io 1");
+        sensors.add("io 2");
+        sensors.add("io 3");
+        ArrayAdapter<String> sensorsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, sensors);
+        sensorsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sensorSpinner = findViewById(R.id.sensorsSpinner);
+        sensorSpinner.setAdapter(sensorsAdapter);
+        sensorSpinner.setSelection(0);
     }
 }
