9d5607bab7b0b6872961ed080c9b9bc950f50d86
Matt Heard <matt@mattheard.net>
2019-05-30 15:14:14 +0200

    Rename method

--- app/src/main/java/net/mattheard/alphabetafilter/MainActivity.java
+++ app/src/main/java/net/mattheard/alphabetafilter/MainActivity.java
@@ -16,10 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setUpSensorSpinner();
+        setUpSensorsSpinner();
     }
 
-    private void setUpSensorSpinner() {
+    private void setUpSensorsSpinner() {
         getSensorsSpinner().setAdapter(getSensorsAdapter());
     }
 
